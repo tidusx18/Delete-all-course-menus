153951841453aa333054a3b2ff0719adceb3bf6d
tidusx18 <victorianowebdesign@gmail.com>
2016-07-20 17:19:57 -0400

    Delete course menus v2
    New version of DeleteCourseMenus that allows page reload in between menu
    deletions. Adds sessionStorage to persist course menu IDs across tab
    reloads.

--- DeleteCourseMenus.js
+++ DeleteCourseMenus.js
@@ -22,27 +22,64 @@ newLi.appendChild(newLink);
 
 document.querySelector(".actionBar").appendChild(newLi);
 
-// Call deleteMenu() when button is clicked
+// Add listener for user click - Call deleteMenu() when clicked
 document.querySelector("#my-button").addEventListener("click", function() {
-    deleteMenu();
+    startDeleteMenus();
 		});
+
+// Only called on user 'click' of delete button
+// Sets sessionStorage variables and faciliates first iteration of course menu deletion process
+function startDeleteMenus() {
+	
+	// First delete confirmation
+	confirm("Careful, you're going to delete all course menus.");
+	
+	// Second and final delete confirmation
+	var confirmation = confirm("This will delete all course menus. No going back after this...Proceed?");
+	
+	// Set sessionStorage variables and data
+	var listItemSpans = document.querySelectorAll("#courseMenuPalette_contents li a span:first-child, #courseMenuPalette_contents li h3 span:first-child");
+	var spantitles = [];
+	for(var i = 0; i<listItemSpans.length; i++) {
+		var title = listItemSpans[i].getAttribute("title");
+		spantitles.push(title);
+	}
+	
+	var listItems = document.querySelectorAll("#courseMenuPalette_contents li");
+	var listItemIds = [];
+	for(var i = 0; i<listItems.length; i++) {
+		var itemId = listItems[i].id.replace("paletteItem:", "");
+		listItemIds.push(itemId);
+	}
+	
+	// Store session data
+	sessionStorage.setItem("counter", "0");
+	sessionStorage.setItem("spantitles", JSON.stringify(spantitles));
+	sessionStorage.setItem("listItemIds", JSON.stringify(listItemIds));
+	
+	// Get session data
+	var storedTitles = sessionStorage.getItem("spantitles");
+	var storedIds = sessionStorage.getItem("listItemIds");
+	
+	// Parse retrieved session data
+	var parsedTitles = JSON.parse(storedTitles);
+	var parsedIds = JSON.parse(storedIds);
+	
+	console.log(parsedTitles);
+	console.log(parsedIds);
+}
 
 // Deletes all existing menu items minus 1 (last child)
 function deleteMenu() {
-    confirm("Careful, you're going to delete all course menus.");
-
-    var confirmation = confirm("This will delete all course menus. No going back after this...Proceed?");
 
     if(confirmation) {
 
-        var listItems = document.querySelectorAll("#courseMenuPalette_contents li");
 
-        var counter = 0;
+        var counter;
 
         if(listItems.length > 0) {
 
             while(counter < listItems.length) {
-                var listItemTitles = document.querySelectorAll("#courseMenuPalette_contents li a span");
 
                 //var title = listItemTitles[counter].getAttribute("title");
 
